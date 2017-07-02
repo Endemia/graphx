a7c541a88eee77cbac7b34786430242655b4c766
Laurent Billiotte <laurent.billiotte@localhost.com>
2017-07-02 18:29:25 +0200

    phyllotaxis et Perlin noise

--- Perlin_noise/cloudy.js
+++ Perlin_noise/cloudy.js
@@ -10,29 +10,29 @@ var inc = 0.01;
 
 var draw = function() {
 
-	var pixels = ctx.createImageData(canvas.width, canvas.height);
+    var pixels = ctx.createImageData(canvas.width, canvas.height);
 
-	var y_perlin = 0;
+    var y_perlin = 0;
 
-	for (var y = 0; y < canvas.height; y++) {
+    for (var y = 0; y < canvas.height; y++) {
 
-		var x_perlin = 0;
+        var x_perlin = 0;
 
-    	for (var x = 0; x < canvas.width; x++) {
-      		var index = (x + y * canvas.width) * 4;
+        for (var x = 0; x < canvas.width; x++) {
+            var index = (x + y * canvas.width) * 4;
 
-      		var value = perlin.noise(x_perlin, y_perlin) * 255;
+            var value = perlin.noise(x_perlin, y_perlin) * 255;
 
 
-      		pixels.data[index + 0] = value;
-      		pixels.data[index + 1] = value;
-      		pixels.data[index + 2] = value;
-      		pixels.data[index + 3] = 255;
+            pixels.data[index + 0] = value;
+            pixels.data[index + 1] = value;
+            pixels.data[index + 2] = value;
+            pixels.data[index + 3] = 255;
 
-      		x_perlin += inc;
-      	}
+            x_perlin += inc;
+        }
 
-      	y_perlin += inc;
+        y_perlin += inc;
     }
 
     ctx.putImageData(pixels, 0, 0);
